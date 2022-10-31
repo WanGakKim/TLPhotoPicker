ff46128a08789dacd6eb172a553c8ef84ba64ee6
WanGak Kim <kimgak22@gmail.com>
2022-10-31 19:50:13 +0900

    선택된 사진 라운드 처리

--- TLPhotoPicker/Classes/TLAutoScaleView.swift
+++ TLPhotoPicker/Classes/TLAutoScaleView.swift
@@ -58,75 +58,29 @@ class TLAutoScaleView: UIView {
     }
 }
 
-class MainContentView: TLAutoScaleView {
+class TLBorderView: TLAutoScaleView, UITextFieldDelegate {
     
-    override func initUI() {
-        self.layer.cornerRadius = 12.5
-        super.initUI()
-    }
-    
-    func setupShadow() {
-        let mainContentView = MainContentView()
-        mainContentView.backgroundColor = .init(white: 0, alpha: 0)
-        self.addSubview(mainContentView)
-        mainContentView.setShadows()
-        //using SnapKit here, you can use NSLayoutConstraint in a similar way to constraint the containerShadow behind your View
-        NSLayoutConstraint.activate([
-            .init(item: mainContentView, attribute: .left,
-                  relatedBy: .equal,
-                  toItem: self, attribute: .left,
-                  multiplier: 1, constant: 0),
-            .init(item: mainContentView, attribute: .right,
-                  relatedBy: .equal,
-                  toItem: self, attribute: .right,
-                  multiplier: 1, constant: 0),
-            .init(item: mainContentView, attribute: .top,
-                  relatedBy: .equal,
-                  toItem: self, attribute: .top,
-                  multiplier: 1, constant: 0),
-            .init(item: mainContentView, attribute: .bottom,
-                  relatedBy: .equal,
-                  toItem: self, attribute: .bottom,
-                  multiplier: 1, constant: 0),
-        ])
-    }
-    
-    private func setShadows(){
-        self.layer.applySketchShadow(color: .init(white: 0, alpha: 0.1),
-                                     alpha: 1,
-                                     x: 0, y: 1.5,
-                                     blur: 2.5, spread: 0)
-    }
-}
-
-class AllStudyTitleView: TLAutoScaleView {
-    
-    override func initUI() {
-        
-        if #available(iOS 11.0, *) {
-            self.layer.cornerRadius = 12.5
-            self.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMaxYCorner]
-        } else {
-            // Fallback on earlier versions
+    @IBInspectable var radius : CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = radius.autoScaled
         }
-        super.initUI()
     }
-}
-
-@IBDesignable
-class StudyManagementHeaderView: TLAutoScaleView {
     
-    override func initUI() {
-        if #available(iOS 11.0, *) {
-            self.layer.cornerRadius = 12.5
-            self.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
-        } else {
-            // Fallback on earlier versions
+    @IBInspectable var width : CGFloat = 1.0 {
+        didSet {
+            self.layer.borderWidth = width
         }
-        super.initUI()
+    }
+    
+    @IBInspectable var color : UIColor = .clear {
+        didSet {
+            self.layer.borderColor = self.color.cgColor
+        }
     }
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        
+        
     }
 }
